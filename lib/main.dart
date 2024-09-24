c72155294db67b4ebe1f58a64b7bf55de0fe54fa
turchaninovvlad <turchaninov-vlad@mail.ru>
2024-09-24 18:38:52 +0300

    1

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(ProductA()); // Убрали const
+}
+
+class ProductA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Магазин товаров',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductListPage(),
+    );
+  }
+}
+
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final String image;
+
+  Product({required this.id, required this.name, required this.description, required this.image});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      image: json['image'],
+    );
+  }
+}
+
+class ProductListPage extends StatefulWidget {
+  @override
+  _ProductListPageState createState() => _ProductListPageState();
+}
+
+class _ProductListPageState extends State<ProductListPage> {
+  List<Product> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadProducts();
+  }
+
+  Future<void> loadProducts() async {
+    final String response = await rootBundle.loadString('data_st/data.json');
+    final List<dynamic> data = json.decode(response);
+    setState(() {
+      products = data.map((json) => Product.fromJson(json)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Список товаров'),
+      ),
+      body: products.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProductDetailPage(product: product),
+                ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue, // Цвет обводки
+                  width: 2.0, // Толщина обводки
+                ),
+                borderRadius: BorderRadius.circular(8), // Скругление углов
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      product.image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(product.name),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ProductDetailPage extends StatelessWidget {
+  final Product product;
+
+  ProductDetailPage({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(product.image),
+            SizedBox(height: 10),
+            Text(
+              product.name,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(product.description),
+          ],
+        ),
+      ),
+    );
+  }
+}
